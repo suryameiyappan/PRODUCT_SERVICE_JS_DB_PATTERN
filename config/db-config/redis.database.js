267a23fea066f5c3f9b5928e0516081ff153aac0
suryameiyappan <137778852+suryameiyappan@users.noreply.github.com>
2026-08-03 16:08:29 +0530

    Update redis.database.js

--- config/db-config/redis.database.js
+++ config/db-config/redis.database.js
@@ -10,7 +10,6 @@ exports.connectRedisClient = () => {
     const redisClient = redis.createClient({
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
-      db: process.env.REDIS_DB,
       password: process.env.REDIS_PASSWORD
     });
     redisClient.on('connect', () => {
